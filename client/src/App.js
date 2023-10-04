b081c8b59e9151d0979e554d54c178cf06c6b286
Akm129 <anitajehu13@gmail.com>
2023-10-05 00:49:38 +0300

    update vet page

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import { SignupForm } from './components/Signup';
+import Veterinarians from './components/Veterinary';
 
 function App() {
   return (
     <div className="App">
         <SignupForm/>
+        <Veterinarians/>
     </div>
   );
 }
