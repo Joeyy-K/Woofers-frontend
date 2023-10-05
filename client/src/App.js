8db01c5068c5bf7802bd91d57ae3976dd8fe45b5
Joseph Karanja <joseph.karanja@student.moringaschool.com>
2023-10-05 23:49:23 +0300

    veterinary

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import Homepage from './components/Homepage';
 import { SignupForm } from './components/Signup';
 import Shop from './components/shop';
 import About from './components/about';
+import Veterinarians from './components/Veterinary';
 
 function App() {
   console.log(window.location)
@@ -16,6 +17,7 @@ function App() {
         <Routes>
           <Route exact="true" path= "/" element={<SignupForm />}/>
           <Route exact="true" path= "/home" element={<Homepage />}/>
+          <Route exact="true" path='/veterinary' element={<Veterinarians />}/>
           <Route exact="true" path= "/shop" element={<Shop />}/>
           <Route exact="true" path= "/about" element={<About />}/>
         </Routes>
